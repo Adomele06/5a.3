dacb9c001a3c1d054f8d0787085a2f01c431bbb4
Domele <adomele@student.tgm.ac.at>
2023-10-21 18:22:32 +0200

    Testen der Klasse WortEintrag

--- src/adomele/Main.java
+++ src/adomele/Main.java
@@ -2,6 +2,11 @@ package adomele;
 
 public class Main {
     public static void main(String[] args) {
-
+        WortEintrag eintrag1 = new WortEintrag("Gym", "https://gym.com");
+        System.out.println(eintrag1.getWort());
+        System.out.println(eintrag1.getUrl());
+        eintrag1.setWort("Keine Gym");
+        System.out.println(eintrag1.checkURL("http://keineGym.com"));
+        System.out.println(eintrag1);
     }
 }
