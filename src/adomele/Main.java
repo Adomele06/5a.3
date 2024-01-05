0387e45369c95105494b8fa4dff9b6181f60f23b
Domele <adomele@student.tgm.ac.at>
2024-01-05 17:38:18 +0100

    Main verbessert

--- src/adomele/Main.java
+++ src/adomele/Main.java
@@ -1,27 +1,59 @@
 package adomele;
-
+import java.io.IOException;
+/**
+ * Klasse Test testet alle Methoden der drei KLassen WortEintrag, -Liste und -Trainer.
+ * @author Alex Domele
+ * @version 2023-09-23
+ */
 public class Main {
     public static void main(String[] args) {
-        WortEintrag eintrag1 = new WortEintrag("Gym", "https://gym.com");
-        System.out.println(eintrag1.getWort());
-        System.out.println(eintrag1.getUrl());
-        eintrag1.setWort("Keine Gym");
-        System.out.println(eintrag1.checkURL("http://keineGym.com"));
-        System.out.println(eintrag1);
+        try {
+            WortEintrag test = new WortEintrag("Gym", "http://a.d");
+            System.out.println(test.checkURL("https://w.a"));
+            System.out.println(test.getWort());
+            System.out.println(test);
+            System.out.println();
+            System.out.println();
+            System.out.println("Zweite Klasse:");
 
-        WortEintrag eintrag2 = new WortEintrag("Auto", "https://auto.at");
-        WortEintrag eintrag3 = new WortEintrag("fahren", "http://fahren.ru");
 
-        WortListe wortListe = new WortListe(10);
-        wortListe.fuegeWortEintragHinzu(eintrag1);
-        wortListe.fuegeWortEintragHinzu(eintrag2);
-        wortListe.fuegeWortEintragHinzu(eintrag3);
-        System.out.println(wortListe);
-        wortListe.loescheWortEintrag("Auto");
-        System.out.println(wortListe);
+            WortListe liste = new WortListe(3);
+            liste.neuerEintrag("Hantel", "http://a.d");
+            liste.neuerEintrag("Stange", "http://a.d");
+            liste.neuerEintrag("Maschine", "https://a.d");
 
-        WortTrainer trainer = new WortTrainer(wortListe);
-        System.out.println(trainer.check("fahren"));
+            System.out.println(liste.getWort(3));
+            System.out.println(liste.loeschEintrag("Katze"));
+            System.out.println(liste.getWort(5));
+            System.out.println("\ntoString: \n" + liste);
 
+            WortTrainer trainer = new WortTrainer(liste);
+            System.out.println("Dritte Klasse: ");
+            System.out.println( trainer.getRandomEintrag());
+            System.out.println(trainer.getAktuell());
+            System.out.println(trainer.check("Hund"));
+            System.out.println(trainer.checkIgnoreCase("Hund"));
+            System.out.println(trainer.statistik());
+
+            trainer.schreiben();
+            trainer.laden();
+            System.out.println(trainer);
+
+
+
+        }
+        catch (NumberFormatException numberFormatException){
+            System.err.println(numberFormatException.getMessage());
+        }
+        catch(IllegalArgumentException illegalArgumentException){
+            System.err.println(illegalArgumentException.getMessage());
+        }
+        catch (NullPointerException nullPointerException){
+            System.err.println(nullPointerException.getMessage());
+        }
+        catch (IOException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
